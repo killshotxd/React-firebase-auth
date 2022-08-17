f39db753e490c7621f13b494c5172ca460c9d5b5
killshotxd <hassanansari211@gmail.com>
2022-08-17 10:34:38 +0530

    login page setup

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styles from "./Login.module.css";
+import InputControl from "../InputControl/InputControl";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerBox}>
+        <h1 className={styles.heading}>Login</h1>
+
+        <InputControl label="Email" placeholder="Enter email" />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
