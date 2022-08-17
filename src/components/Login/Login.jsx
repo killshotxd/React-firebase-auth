1ab55fedccd066639305af652edd9f4cadd431d2
killshotxd <hassanansari211@gmail.com>
2022-08-17 10:39:56 +0530

    login jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Login.module.css";
 import InputControl from "../InputControl/InputControl";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
@@ -9,9 +10,22 @@ const Login = () => {
         <h1 className={styles.heading}>Login</h1>
 
         <InputControl label="Email" placeholder="Enter email" />
+        <InputControl label="Password" placeholder="Enter password" />
+
+        <div className={styles.footer}>
+          <button>Login</button>
+          <p>
+            Already Have an account?{" "}
+            <span>
+              <Link to="/SignUp">Sign Up</Link>
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Login;
+
+// 16:38m
