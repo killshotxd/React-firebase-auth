79a913009a06c76078fee972b21777179560fb1c
killshotxd <hassanansari211@gmail.com>
2022-08-18 09:44:45 +0530

    signup functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/SignUp";
+import { auth } from "./Firebase";
 
 const App = () => {
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserName(user.displayName);
+      } else setUserName("");
+    });
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/SignUp" element={<SignUp />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home name={userName} />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,20 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Signup.module.css";
 import InputControl from "../InputControl/InputControl";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "../../Firebase";
+import { async } from "@firebase/util";
 
 const Login = () => {
+  const Navigate = useNavigate();
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    pass: "",
+  });
+
+  const [submitBtnDisabled, setSubmitBtnDisabled] = useState(false);
+
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const handleSubmission = () => {
+    if (!values.name || !values.email || !values.pass) {
+      setErrorMsg("Fill all the fields...");
+      return;
+    }
+    setErrorMsg("");
+
+    setSubmitBtnDisabled(true);
+    createUserWithEmailAndPassword(auth, values.email, values.pass)
+      .then(async (res) => {
+        setSubmitBtnDisabled(false);
+        const user = res.user;
+        await updateProfile(user, {
+          displayName: values.name,
+        });
+        Navigate("/");
+      })
+      .catch((err) => {
+        setSubmitBtnDisabled(false);
+        setErrorMsg(err.message);
+      });
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.innerBox}>
         <h1 className={styles.heading}>Login</h1>
 
-        <InputControl label="Name" placeholder="Enter name" />
-        <InputControl label="Email" placeholder="Enter email" />
-        <InputControl label="Password" placeholder="Enter password" />
+        <InputControl
+          label="Name"
+          placeholder="Enter name"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, name: event.target.value }))
+          }
+        />
+        <InputControl
+          label="Email"
+          placeholder="Enter email"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, email: event.target.value }))
+          }
+        />
+        <InputControl
+          label="Password"
+          placeholder="Enter password"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, pass: event.target.value }))
+          }
+        />
 
         <div className={styles.footer}>
-          <button>SignUp</button>
+          <b className={styles.error}>{errorMsg}</b>
+          <button onClick={handleSubmission} disabled={submitBtnDisabled}>
+            SignUp
+          </button>
           <p>
             Already Have an account?{" "}
             <span>
