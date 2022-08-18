51c45ddc0bdfe7a133ec5214b3bb4ab8b3475a25
killshotxd <hassanansari211@gmail.com>
2022-08-18 09:08:33 +0530

    UI complete

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import styles from "./Signup.module.css";
+import InputControl from "../InputControl/InputControl";
+import { Link } from "react-router-dom";
 
-const SignUp = () => {
-  return <div>SignUp</div>;
+const Login = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerBox}>
+        <h1 className={styles.heading}>Login</h1>
+
+        <InputControl label="Name" placeholder="Enter name" />
+        <InputControl label="Email" placeholder="Enter email" />
+        <InputControl label="Password" placeholder="Enter password" />
+
+        <div className={styles.footer}>
+          <button>SignUp</button>
+          <p>
+            Already Have an account?{" "}
+            <span>
+              <Link to="/login">Login</Link>
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default SignUp;
+export default Login;
+
+// 16:38m
