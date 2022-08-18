3ec74b3526c51a1435bd882202258821402dfd1d
killshotxd <hassanansari211@gmail.com>
2022-08-18 09:54:01 +0530

    Login functionality

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Login.module.css";
 import InputControl from "../InputControl/InputControl";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "../../Firebase";
+import { async } from "@firebase/util";
 
 const Login = () => {
+  const Navigate = useNavigate();
+  const [values, setValues] = useState({
+    email: "",
+    pass: "",
+  });
+
+  const [submitBtnDisabled, setSubmitBtnDisabled] = useState(false);
+
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const handleSubmission = () => {
+    if (!values.email || !values.pass) {
+      setErrorMsg("Fill all the fields...");
+      return;
+    }
+    setErrorMsg("");
+
+    setSubmitBtnDisabled(true);
+    signInWithEmailAndPassword(auth, values.email, values.pass)
+      .then(async (res) => {
+        setSubmitBtnDisabled(false);
+
+        Navigate("/");
+      })
+      .catch((err) => {
+        setSubmitBtnDisabled(false);
+        setErrorMsg(err.message);
+      });
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.innerBox}>
         <h1 className={styles.heading}>Login</h1>
 
-        <InputControl label="Email" placeholder="Enter email" />
-        <InputControl label="Password" placeholder="Enter password" />
+        <InputControl
+          label="Email"
+          placeholder="Enter email"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, email: event.target.value }))
+          }
+        />
+        <InputControl
+          label="Password"
+          placeholder="Enter password"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, pass: event.target.value }))
+          }
+        />
 
         <div className={styles.footer}>
-          <button>Login</button>
+          <b className={styles.error}>{errorMsg}</b>
+          <button disabled={submitBtnDisabled} onClick={handleSubmission}>
+            Login
+          </button>
           <p>
             Already Have an account?{" "}
             <span>
